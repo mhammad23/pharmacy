a9c4705b84214b30363558636399460d21cd36bb
mhammad23 <mh12190609@gmail.com>
2025-09-26 09:15:06 +0300

    apps adjustments

--- pharamacy_project/pharamacy_project/settings.py
+++ pharamacy_project/pharamacy_project/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users',
+    'doctors',
+    'suppliers',
+    'pharmacies',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
